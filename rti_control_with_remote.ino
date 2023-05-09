45e0dc6a49e0d41532e2ca129aa2007a4231fb4b
TymEK49 <tym.bla@gmail.com>
2023-05-09 23:28:15 +0200

    New app for ardino with remote
    - added OE remote support
    - added saving last state
    - changed USB commands
    - fixed brightness array
    - fixed typos

---
+++ rti_control_with_remote.ino
@@ -0,0 +1,186 @@
+//  Have fun! Greetings from Tymek Be
+//  Whole tutorial and 'how to' on
+//  https://github.com/TymEK49/RTI_control
+//
+//  Connectors: 
+//  RX  -> 5 pin RTI
+//  TX  -> 4 pin RTI
+//  GND -> 7 pin RTI
+
+
+#include <EEPROM.h>
+//EEPROM index: 0 RTI mode, 1 default brightness, 2 enter key brightness, 3 back key brightness
+
+String ab[10];
+String data;
+String sectionData;
+int a;
+int b;
+int stringData;
+int last_RTI_mode = 2;
+int last_RTI_brightness = 15;
+int enter_brightness = 15;
+int back_brightness = 3;
+int index_brightness;
+const int BUFFER_SIZE = 4;
+const long DEBOUNCE_TIME_MILS = 750;
+char remote_signal[BUFFER_SIZE];
+char last_signal[BUFFER_SIZE];
+long last_signal_time = 0;
+char screen_state[]       = {0x40, 0x45, 0x4C, 0x46}; // RTI_RGB, RTI_PAL, RTI_NTSC, RTI_OFF
+char codes_state[]        = {0x72, 0x70, 0x6E, 0x6F}; //keys r, p, n, o
+char screen_brightness[]  = {0x20, 0x61, 0x62, 0x23, 0x64, 0x25, 0x26, 0x67, 0x68, 0x29, 0x2A, 0x6B, 0x2C, 0x6D, 0x6E, 0x2F}; // brightness from 0 to 15
+char codes_brightness[]   = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66}; //keys 0-9, a, b, c, d, e, f
+char up1[BUFFER_SIZE]     = {0xED, 0xAF, 0xBD, 0xFD}; 
+char up2[BUFFER_SIZE]     = {0xED, 0xAF, 0xFD, 0xFD}; 
+char up3[BUFFER_SIZE]     = {0xED, 0xEF, 0xBD, 0xFD}; 
+char down1[BUFFER_SIZE]   = {0xED, 0xAD, 0xAF, 0xFF}; 
+char down2[BUFFER_SIZE]   = {0xED, 0xAD, 0xEF, 0xFF}; 
+char left1[BUFFER_SIZE]   = {0xED, 0xBD, 0xBD, 0xFD}; 
+char left2[BUFFER_SIZE]   = {0xED, 0xBD, 0xFD, 0xFD}; 
+char left3[BUFFER_SIZE]   = {0xED, 0xFD, 0xBD, 0xFD}; 
+char right1[BUFFER_SIZE]  = {0xED, 0xEF, 0xAF, 0xED}; 
+char right2[BUFFER_SIZE]  = {0xED, 0xEF, 0xEF, 0xED}; 
+char enter[BUFFER_SIZE]   = {0xED, 0xEF, 0xED, 0xED}; 
+char back1[BUFFER_SIZE]   = {0xED, 0xBD, 0xEF, 0xED}; 
+char back2[BUFFER_SIZE]   = {0xED, 0xFD, 0xEF, 0xED}; 
+char signal_state;
+char signal_brightness;
+
+
+void setup()
+{
+  Serial.begin(2400);
+  checkPowerState();
+  checkBrightnessState();
+  checkEnterState();
+  checkBackState();
+}
+  
+void loop()
+{
+  Serial.print(signal_state);
+  delay(50);
+  Serial.print(signal_brightness); // {0x20, 0x61, 0x62, 0x23, 0x64, 0x25, 0x26, 0x67, 0x68, 0x29, 0x2A, 0x2C, 0x6B, 0x6D, 0x6E, 0x2F};
+  delay(50);
+  Serial.print((char)0x83); // must have, volvo things
+  delay(50);
+  if (Serial.available() > 0) {
+      int rlen = Serial.readBytesUntil('\n', remote_signal, sizeof(remote_signal));
+      debounceSignal(remote_signal);
+  }
+}
+
+void checkPowerState() {
+  if (EEPROM.read(0) != NAN){  
+    last_RTI_mode = EEPROM.read(0);
+    signal_state = screen_state[last_RTI_mode];
+  }
+  else {
+    EEPROM.update(0, last_RTI_mode);
+  }
+}
+
+void checkBrightnessState() {
+  if (EEPROM.read(1) != NAN){  
+    last_RTI_brightness = EEPROM.read(1);
+    index_brightness = last_RTI_brightness;
+    signal_brightness = screen_brightness[index_brightness];
+  }
+  else {
+    EEPROM.update(1, last_RTI_brightness);
+  }
+}
+
+void checkEnterState() {
+  if (EEPROM.read(2) != NAN){
+    enter_brightness = EEPROM.read(2);    
+  }
+  else {
+    EEPROM.update(2, enter_brightness);
+  }
+}
+
+void checkBackState() {
+  if (EEPROM.read(3) != NAN){
+    back_brightness = EEPROM.read(3);    
+  }
+  else {
+    EEPROM.update(3, back_brightness);
+  }
+}
+
+void debounceSignal(char signal[4]) {
+    if (millis() - last_signal_time < DEBOUNCE_TIME_MILS)
+        return;
+    memcpy(last_signal, signal, 4);
+    last_signal_time = millis();
+    handleSignal(remote_signal);
+}
+
+void handleSignal(char signal[4]) {
+    if (memcmp(signal, up1, 4) == 0 || memcmp(signal, up2, 4) == 0 || memcmp(signal, up3, 4) == 0){
+      signal_state = screen_state[2];
+      EEPROM.update(0, 2);
+    }
+    if (memcmp(signal, down1, 4) == 0 || memcmp(signal, down2, 4) == 0) {
+      signal_state = screen_state[3];
+      EEPROM.update(0, 3);
+    }
+    if (memcmp(signal, left1, 4) == 0 || memcmp(signal, left2, 4) == 0 || memcmp(signal, left3, 4) == 0){
+      if (index_brightness > 0 && index_brightness < 16){
+        index_brightness = index_brightness - 1;
+        signal_brightness = screen_brightness[index_brightness];
+        EEPROM.update(1, index_brightness);
+      }
+    }
+    if (memcmp(signal, right1, 4) == 0 || memcmp(signal, right2, 4) == 0) {
+      if (index_brightness >= 0 && index_brightness < 15){
+        index_brightness = index_brightness + 1;
+        signal_brightness = screen_brightness[index_brightness];
+        EEPROM.update(1, index_brightness);
+      }
+    }
+    if (memcmp(signal, enter, 4) == 0) {
+      index_brightness = enter_brightness;
+      signal_brightness = screen_brightness[index_brightness];
+      EEPROM.update(1, index_brightness);
+    }
+    if (memcmp(signal, back1, 4) == 0 || memcmp(signal, back2, 4) == 0) {
+      index_brightness = back_brightness;
+      signal_brightness = screen_brightness[index_brightness];
+      EEPROM.update(1, index_brightness);
+    }
+    else{
+      commandsUSB(signal);
+    }
+}
+
+void commandsUSB(char signal[4]) {  
+  for (b = 0; b < 4; b++){
+    if (signal[0] == codes_state[b]){
+      signal_state = screen_state[b];
+      EEPROM.update(0, b);
+    }
+  }
+  for (b = 0; b < 16; b++){
+      if (signal[0] == codes_brightness[b]){
+        signal_brightness = screen_brightness[b];
+        EEPROM.update(1, b);
+      }
+    }
+  if (signal[0] == char(0x78)){
+  // sending 'x' key will set RTI in off after next boot, this is for android development purpose  
+    EEPROM.update(0, index_brightness);
+  }
+  if (signal[0] == char(0x2B)){
+  // sending '+' key will set current brightness as ENTER key
+    enter_brightness = index_brightness;
+    EEPROM.update(2, index_brightness);
+  }
+  if (signal[0] == char(0x2D)){
+  // sending '-' key will set current brightness as BACK key  
+    back_brightness = index_brightness;
+    EEPROM.update(3, index_brightness);
+  }
+}
